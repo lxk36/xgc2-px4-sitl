463d79c0dfc88abde82224120c7599dd88ffffd6
Lxk_buaa <867768510@qq.com>
2026-05-31 21:09:10 +0800

    Install PX4 Python build dependencies

--- scripts/build_runtime_deb_in_docker.sh
+++ scripts/build_runtime_deb_in_docker.sh
@@ -77,9 +77,15 @@ docker run --rm \
       ninja-build \
       python3 \
       python3-dev \
+      python3-empy \
+      python3-jinja2 \
+      python3-numpy \
+      python3-packaging \
       python3-pip \
       python3-setuptools \
+      python3-toml \
       python3-wheel \
+      python3-yaml \
       ros-jazzy-ros2pkg \
       rsync \
       sudo \
